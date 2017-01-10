39a6ea257f7ffaf2338b77caea3746ece147bbfc
houssemtayech <contact@houssemtayech.com>
2017-01-10 04:07:46 +0100

    ajout de l'exception ($date1 > $date2)

--- src/Date/AppBundle/Controller/DefaultController.php
+++ src/Date/AppBundle/Controller/DefaultController.php
@@ -12,6 +12,7 @@ class DefaultController extends Controller
         $form = $this->createForm(new SearchFormType());
         $request = $this->getRequest();
         $jours_feries[] = array();
+
         $jours_feries= ['0','106','120','127','144','155','194','226','304','314','358'];
         if ($request->getMethod() == 'POST') {
             $form->bind($request);
@@ -22,38 +23,46 @@ class DefaultController extends Controller
                 $mercredi_sur_deux = false;
                 $nbre_mercredi = 0;
                 $nbre_des_jours_feries = 0;
-                foreach (new \DatePeriod( $date1, new \DateInterval('P1D'), $date2) as $jour)
-                {
-                    $num_du_jour = $jour->format('N');
-                    $num_du_jour2 = $jour->format('z');
-                    if (($num_du_jour != '6') && ($num_du_jour != '7'))
-                    {
-                        ++$nbre_des_jours;
-                        if(($num_du_jour == '3') && ($mercredi_sur_deux==false) )
-                        {
-                            ++$nbre_mercredi;
-                            $mercredi_sur_deux=true;
-                        }
-                        elseif (($num_du_jour == '3') && ($mercredi_sur_deux==true))
-                        {
-                            $mercredi_sur_deux = false;
 
-                        }
-                        foreach ($jours_feries as $un_jour_ferie)
+                if($date1<$date2)
+                {
+                    foreach (new \DatePeriod( $date1, new \DateInterval('P1D'), $date2) as $jour)
+                    {
+                        $num_du_jour = $jour->format('N');
+                        $num_du_jour2 = $jour->format('z');
+                        if (($num_du_jour != '6') && ($num_du_jour != '7'))
                         {
-                            if (($un_jour_ferie == $num_du_jour2) )
+                            ++$nbre_des_jours;
+                            if(($num_du_jour == '3') && ($mercredi_sur_deux==false) )
                             {
-                                ++$nbre_des_jours_feries;
+                                ++$nbre_mercredi;
+                                $mercredi_sur_deux=true;
+                            }
+                            elseif (($num_du_jour == '3') && ($mercredi_sur_deux==true))
+                            {
+                                $mercredi_sur_deux = false;
+
+                            }
+                            foreach ($jours_feries as $un_jour_ferie)
+                            {
+                                if (($un_jour_ferie == $num_du_jour2) )
+                                {
+                                    ++$nbre_des_jours_feries;
+                                }
+
                             }
 
+
                         }
-
-
                     }
+                    $nbre_des_jours = $nbre_des_jours + 1 - $nbre_mercredi - $nbre_des_jours_feries;
+                    $resultat = "La différence est de: " .$nbre_des_jours. " jours ou on a dans cette période: " .$nbre_mercredi. " mercredi sur deux et: ".$nbre_des_jours_feries. " jours fériés";
                 }
-                $nbre_des_jours = $nbre_des_jours + 1 - $nbre_mercredi - $nbre_des_jours_feries;
+                else
+                    $resultat = "Erreur, Date 1 >= Date 2";
 
-                return $this->render('DateAppBundle:Default:resultat.html.twig', array('resultat' => $nbre_des_jours));
+
+                return $this->render('DateAppBundle:Default:resultat.html.twig', array('resultat' => $resultat));
             }
         }
 
